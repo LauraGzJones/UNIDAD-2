57367982293c49427060f26a1748464cbf2f5954
LauraGzJones <lauragzjones@gmail.com>
2026-04-08 20:33:43 -0400

    ok

--- 07_04_2026_4.py
+++ 07_04_2026_4.py
@@ -12,7 +12,7 @@ numero_1 = int(input("Ingresa el primer número: "))
 numero_2 = int(input("Ingresa el segundo número: "))
 numero_3 = int(input("Ingresa el tercer número: "))
 
-# Paso 1: ordenar los dos primeros
+# ordenar los dos primeros de menos a mas 
 if numero_1 < numero_2:
     menor = numero_1
     mayor = numero_2
@@ -20,12 +20,12 @@ else:
     menor = numero_2
     mayor = numero_1
 
-# Paso 2: ubicar el tercero
+# comparar con el numero 3  
 if numero_3 < menor:
-    print(numero_3, menor, mayor)
+    print( "Numeros ordenados de menor a mayor :", numero_3, menor, mayor)
 else:
     if numero_3 > mayor:
-        print(menor, mayor, numero_3)
+        print("Numeros ordenados de menor a mayor:",menor, mayor, numero_3)
     else:
-        print(menor, numero_3, mayor)
+        print("Numeros ordenados de menor a mayor:", menor, numero_3, mayor)
 
