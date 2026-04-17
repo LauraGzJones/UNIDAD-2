464db904a59cfea0da9ce76503da95a111625eb2
LauraGzJones <lauragzjones@gmail.com>
2026-04-17 19:40:14 -0400

    ok

--- clase15-04-2026/eje_3.py
+++ clase15-04-2026/eje_3.py
@@ -4,13 +4,14 @@ al final tiene que desplegar la suma de estos'''
 #declara variables 
 x  = 1
 suma = 0 
-n_ux = int (input("Ingresa "))
-suma = 0 
-
-
-
-n_ux = int (input ("Ingresa el numero" ))
-suma 
-
+ 
+while x <= 5:   # es un ciclo.
+    num = int(input("Ingresa numero[" + str(x) + "de 5] :"))
+    # es el mensaje que se muestra.
+ #str(x) → convierte x a texto (porque no puedes juntar número + texto directamente).
+    suma = suma + num
+    x = x + 1 #es la condición.
+print ("suma :", suma)    
+ 
 
 
